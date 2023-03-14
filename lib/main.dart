bfcec31fe794283922dc2931080849d89ebee71d
shardhayv <shardhayvatshyayan7@gmail.com>
2023-03-14 11:25:20 +0545

    xylophone done

--- lib/main.dart
+++ lib/main.dart
@@ -16,26 +16,26 @@ class Xylophone extends StatelessWidget {
         body: SafeArea(
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
+                Center(
                   child: Center(
                     child: TextButton(
                       onPressed: () async {
                         final player = AudioPlayer();
                         await player.play(
-                          AssetSource('music/mus.mp3'),
+                          AssetSource('note1.wav'),
                         );
-
                       },
                       style: TextButton.styleFrom(
-                        foregroundColor: Colors.black,
-                        backgroundColor: Colors.black,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                        // foregroundColor: Colors.black,
+                        backgroundColor: Colors.red,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
                       ),
                       child: const Text(
-                        'Start!',
+                        'Sa!',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 20,
@@ -44,10 +44,164 @@ class Xylophone extends StatelessWidget {
                     ),
                   ),
                 ),
-
-
+                const SizedBox(height: 30),
+                Center(
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () async {
+                        final player = AudioPlayer();
+                        await player.play(
+                          AssetSource('note2.wav'),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        // foregroundColor: Colors.black,
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                      ),
+                      child: const Text(
+                        'Re!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Center(
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () async {
+                        final player = AudioPlayer();
+                        await player.play(
+                          AssetSource('note3.wav'),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        // foregroundColor: Colors.black,
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                      ),
+                      child: const Text(
+                        'Ga!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Center(
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () async {
+                        final player = AudioPlayer();
+                        await player.play(
+                          AssetSource('note4.wav'),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        // foregroundColor: Colors.black,
+                        backgroundColor: Colors.purpleAccent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                      ),
+                      child: const Text(
+                        'Ma!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Center(
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () async {
+                        final player = AudioPlayer();
+                        await player.play(
+                          AssetSource('note5.wav'),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        // foregroundColor: Colors.black,
+                        backgroundColor: Colors.tealAccent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                      ),
+                      child: const Text(
+                        'Pa!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Center(
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () async {
+                        final player = AudioPlayer();
+                        await player.play(
+                          AssetSource('note6.wav'),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        // foregroundColor: Colors.black,
+                        backgroundColor: Colors.redAccent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                      ),
+                      child: const Text(
+                        'Dha!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Center(
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () async {
+                        final player = AudioPlayer();
+                        await player.play(
+                          AssetSource('note7.wav'),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        // foregroundColor: Colors.black,
+                        backgroundColor: Colors.teal,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                      ),
+                      child: const Text(
+                        'Ni!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
               ],
-
             ),
           ),
         ),
